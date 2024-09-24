12f569d07b3435aa382761b1f7e3edaeea61b890
Miguelkohi <miguelcorde25@gmail.com>
2024-09-24 15:19:22 -0300

    a

---
+++ src/components/faixa/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+
+export default function Faixa(){
+  return (
+    <div className="faixa">
+        <h1>{Faixa.titulo}</h1>
+    </div>
+  );
+};
+
--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,4 +1,5 @@
 import Cabecalho from '../../components/cabecalho';
+import Faixa from '../../components/faixa';
 import './index.scss';
 
 
@@ -8,6 +9,32 @@ export default function App() {
       <Cabecalho></Cabecalho>
       <section className='slider'>
         <img src="/assets/images/clinica-odontologica.jpg" alt="" />
+      </section>
+      <div className="faixa">
+        <h1>Nossos Serviços</h1>
+      </div>
+      <div className='linha'>
+        <nav className="sevicos">
+          <ul className="listaServicos">
+            <li className="servicoItem"><a href="#">Implate Dentário</a></li>
+            <li className="servicoItem"><a href="#">Implate Dentário</a></li>
+            <li className="servicoItem"><a href="#">Implate Dentário</a></li>
+            <li className="servicoItem"><a href="#">Implate Dentário</a></li>
+            <li className="servicoItem"><a href="#">Implate Dentário</a></li>
+          </ul>
+        </nav>
+      </div>
+      <section className='secao1'>
+        <img src="/assets/images/foto.jpg" alt="" />
+        <p>
+        Na OdontoClarc, oferecemos cuidados odontológicos de qualidade superior, combinando tecnologia moderna e um atendimento caloroso. Nossa equipe especializada proporciona uma ampla gama de serviços, desde limpezas preventivas até tratamentos ortodônticos avançados, garantindo resultados duradouros e satisfatórios.        
+        </p>
+      </section>
+      <section className='secao2'>
+        <p>
+        Na OdontoClarc, oferecemos cuidados odontológicos de qualidade superior, combinando tecnologia moderna e um atendimento caloroso. Nossa equipe especializada proporciona uma ampla gama de serviços, desde limpezas preventivas até tratamentos ortodônticos avançados, garantindo resultados duradouros e satisfatórios.        
+        </p>
+        <img src="/assets/images/foto.jpg" alt="" />
       </section>
     </div>
   );

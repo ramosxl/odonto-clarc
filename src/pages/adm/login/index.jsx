88a688deb2173a2193f8e77f5b1a8d98da8c84e9
Miguelkohi <ra54274284883@nsfatima.org.br>
2024-10-21 18:22:33 -0300

    .

--- src/pages/adm/login/index.jsx
+++ src/pages/adm/login/index.jsx
@@ -5,15 +5,18 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 import Footer from '../../../components/footer/index.jsx';
 import FooterADM from '../../../components/footerAdm/index.jsx';
+import { IoChevronBackOutline } from "react-icons/io5";
+
 
 export default function Login() {
   return (
     <div className="Login">
       <Cabecalho_ADM />
       <Link to="/">
-      <button type="button" className='voltar'>Voltar</button>
+      <button type="button" className='voltar'><IoChevronBackOutline />Voltar</button>
       </Link>
       <div className="fundo">
+        <h1>Login ADM</h1>
         <input type="text" className="text" placeholder="Login" />
         <input type="password" className="text" placeholder="Senha" />
         <button type="button" className='enviar'>Entrar</button>

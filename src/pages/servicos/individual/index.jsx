e3aa38e8e0d4957044442311dca837d7722773a1
Miguelkohi <ra54274284883@nsfatima.org.br>
2024-10-22 14:39:28 -0300

    .

---
+++ src/pages/servicos/individual/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import Cabecalho from '../../components/cabecalho';
+import Faixa from '../../components/faixa';
+import './index.scss';
+import { Link } from 'react-router-dom';
+import Footer from '../../components/footer/index.jsx';
+
+export default function Contato() {
+  const [minDate, setMinDate] = useState('');
+
+
+  return (
+    <div className="Contato">
+      <Cabecalho />
+      <Link to='/'>
+        <button type="button" className='voltar'>Voltar</button>
+      </Link>
+      <div className="fundo">
+        <div className="esq">
+          <input type="text" className="text" placeholder="Nome" />
+          <input type="text" className="text" placeholder="Sobrenome" />
+          <input type="text" className="text" placeholder="E-mail" />
+          <input type="text" className="text" placeholder="Telefone" />
+          <div className="input-data">
+            <input
+              type="date"
+              className="date"
+              min={minDate}
+              id="dateInput"
+            />
+          </div>
+          <textarea className="msg" placeholder="Deixe sua mensagem" rows="4"></textarea>
+
+          <button type="button" className='enviar'>Enviar</button>
+        </div>
+        <div className="dir">
+          <img src="/assets/images/odc-min.png" alt="" />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
